f8a181702b156d509ab674ca188661fd76566951
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-07-27 11:30:36 -0700

    added new branch

--- hangman.c
+++ hangman.c
@@ -8,39 +8,34 @@
 
 int main()
 {
-	int a = 0;
 	char word[] = "apple";
 	int penalty = 0;
-	int i = 65;
+	int i = 0;
 
 	printf("[%s]\nLength of string: %ld\n", word, sizeof(word));
 
 	char ch;
 
-	do
+	printf("\n");
+	printf("Enter letter to search: ");
+	scanf("%c", &ch);
+
+	for (i = 0; i < 26; i++) // If letter is in word, all instances revealed
 	{
-		printf("\n");
-		printf("Enter letter to search: ");
-		scanf("%c", &ch);
-
-		for (i = 0; i < strlen(word); i++) // If letter is in word, all instances revealed
+		if (word[i] == ch) // Was found in Search
 		{
-			if (word[i] == ch) // Was found in Search
-			{
-				printf("Found value: %c = %d", i, ch);
-				break;
-			}
-
-			else
-			{
-				printf("\nValue Not found: %c = %d\n", i, ch);
-				++penalty; // Letter not found, penalty is added
-				break;
-			}	
-			printf("\n");
+			printf("Found value: %c = %d", i, ch);
+			break;
 		}
-		a = a + 1;
-	} while (a <= GUESS_COUNT);
+
+		else
+		{
+			printf("\nValue Not found: %c = %d\n", i, ch);
+			++penalty; // Letter not found, penalty is added
+			break;
+		}	
+		printf("\n");
+	}
 
 	return 0;
 }
